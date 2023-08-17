f4f1b782dc4263728fec68639e7c1efe9b770a63
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-17 16:14:20 +0300

    add instance fields

--- src/main/java/org/example/Fields.java
+++ src/main/java/org/example/Fields.java
@@ -2,5 +2,10 @@ package org.example;
 
 public class Fields {
     public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.name = "Buddy";
+        Dog dog2 = new Dog();
+        dog2.name = "Charlie";
+        System.out.println(dog1.name + " + " + dog2.name);
     }
 }
