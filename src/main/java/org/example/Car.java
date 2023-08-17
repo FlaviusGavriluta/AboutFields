013ce12ce5b9523e33a056312492d5d25785fb08
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-17 16:39:02 +0300

    add attributes

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Car {
+    String color; // field representing the color attribute
+    String brand; // field representing the brand attribute
+    String engineType; // field representing the engineType attribute
+    int speed; // field representing the speed attribute
+}
